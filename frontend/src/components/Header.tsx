bcaf67cd6342ec8cbbc521f22146c5862a85cb57
Amri Mufti <amrimuvti@gmail.com>
2024-08-08 00:19:17 +0700

    test husky

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,9 @@
 const Header = () => {
-	return (
-		<header className="bg-blue-600 text-white p-4">
-			<h1 className="text-2xl font-bold">My App</h1>
-		</header>
-	);
+  return (
+    <header className="bg-blue-600 text-white p-4">
+      <h1 className="text-2xl font-bold">HompimPOS</h1>
+    </header>
+  );
 };
 
 export default Header;
