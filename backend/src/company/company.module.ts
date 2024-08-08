1f73fbe7b06016e508bed3f3e74a80a1b8ce9678
Amri Mufti <amrimuvti@gmail.com>
2024-08-08 14:34:18 +0700

    fix(company): provide Logger in the CompanyModule

--- backend/src/company/company.module.ts
+++ backend/src/company/company.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyService } from './company.service';
 import { CompanyController } from './company.controller';
@@ -7,6 +7,6 @@ import { Company } from './company.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([Company])],
   controllers: [CompanyController],
-  providers: [CompanyService],
+  providers: [CompanyService, Logger],
 })
 export class CompanyModule {}
